37d01062aa0760f11b4bca70cc54266775d03163
ehuntober <ehuntercrypto@gmail.com>
2024-05-05 20:36:07 +0100

    updated the user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,32 +1,20 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password:{
-        type:String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    }
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  url: { type: String, required: true, unique: true },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')) return next()
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next();
-})
-
-
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
