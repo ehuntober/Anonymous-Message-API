5ef94894e3d5cb1fba16bc9485e603d399b26878
ehuntober <ehuntercrypto@gmail.com>
2024-05-07 15:38:17 +0100

    updated the password controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/refresh-token', authController.refreshToken);
 
 module.exports = router;
